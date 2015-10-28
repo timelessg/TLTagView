9aef50df305a2b1b1f5ec48ac04a1949f23a01ea
Garry <timelessg@hotmail.com>
2015-10-28 23:50:34 +0800

    coding

--- TLTag/TLTag.m
+++ TLTag/TLTag.m
@@ -14,11 +14,10 @@
 
 @interface TLTag ()
 @property(nonatomic,strong)NSMutableArray *tags;
-@property(nonatomic,strong)NSMutableArray *tagViews;
+@property(nonatomic,strong)NSMutableArray *tagButtons;
 @property(nonatomic,assign)TLTagMode mode;
 
 @property(nonatomic,strong)TLTextField *enterTextfield;
-@property(nonatomic,strong)UIView *selectedTagView;
 @end
 
 @implementation TLTag
@@ -46,26 +45,26 @@
 }
 -(void)initTagsView
 {
-    self.tagViews = [@[] mutableCopy];
-    UIView *lastTagView = nil;
+    self.tagButtons = [@[] mutableCopy];
+    UIButton *lastTagButton = nil;
     CGFloat tagsWidth = 0;
     NSUInteger lineCount = 0;
     NSUInteger tmpLineCount = 0;
     BOOL isBreak = NO;
     for (int i = 0; i < self.tags.count; i ++) {
-        UIView *tagView = [self tagWithText:self.tags[i]];
+        UIButton *tagView = [self tagButtonWithText:self.tags[i]];
         [self addSubview:tagView];
         [tagView mas_makeConstraints:^(MASConstraintMaker *make) {
-            if (lastTagView)
+            if (lastTagButton)
             {
                 if (isBreak)
                 {
                     make.left.equalTo(self.mas_left).offset(0);
-                    make.top.equalTo(lastTagView.mas_bottom).offset(5);
+                    make.top.equalTo(lastTagButton.mas_bottom).offset(5);
                 }else
                 {
-                    make.left.equalTo(lastTagView.mas_right).offset(5);
-                    make.top.equalTo(lastTagView.mas_top).offset(0);
+                    make.left.equalTo(lastTagButton.mas_right).offset(5);
+                    make.top.equalTo(lastTagButton.mas_top).offset(0);
                 }
             }else
             {
@@ -87,109 +86,118 @@
                 isBreak = NO;
             }
         }
-        lastTagView = tagView;
-        [self.tagViews addObject:tagView];
+        lastTagButton = tagView;
+        [self.tagButtons addObject:tagView];
     }
     if (self.mode == TLTagModeEdit)
     {
-        self.enterTextfield = [[TLTextField alloc] init];
-        self.enterTextfield.font = [UIFont systemFontOfSize:12];
         [self addSubview:self.enterTextfield];
         [self.enterTextfield mas_makeConstraints:^(MASConstraintMaker *make)
-        {
-            if (lastTagView)
-            {
-                make.left.mas_equalTo(lastTagView.mas_right).offset(5);
-                make.top.mas_equalTo(lastTagView.mas_top).offset(0);
-                make.height.equalTo(lastTagView.mas_height);
-            }else
-            {
-                make.left.equalTo(self.mas_left).offset(0);
-                make.top.equalTo(self.mas_top).offset(0);
-                make.height.mas_equalTo(@30);
-            }
-            make.width.mas_equalTo(self.frame.size.width / 2);
-        }];
-        WS(weakSelf);
-        self.enterTextfield.backward = ^(){
-            if ([weakSelf.enterTextfield.text isEqualToString:@""])
-            {
-                [[weakSelf getLastTagView] removeFromSuperview];
-                [weakSelf.tagViews removeLastObject];
-                [weakSelf reLayoutTextField];
-            }
-        };
-        self.enterTextfield.didReturn = ^(NSString *text){
-            [weakSelf insterTagWith:text];
-        };
+         {
+             if (lastTagButton)
+             {
+                 make.left.mas_equalTo(lastTagButton.mas_right).offset(5);
+                 make.top.mas_equalTo(lastTagButton.mas_top).offset(0);
+                 make.height.equalTo(lastTagButton.mas_height);
+             }else
+             {
+                 make.left.equalTo(self.mas_left).offset(0);
+                 make.top.equalTo(self.mas_top).offset(0);
+                 make.height.mas_equalTo(@30);
+             }
+             make.width.mas_equalTo(self.frame.size.width / 2);
+         }];
     }
 }
--(UIView *)tagWithText:(NSString *)text
+-(UIButton *)tagButtonWithText:(NSString *)text
 {
-    UIView *tagView = [[UIView alloc] init];
-    tagView.userInteractionEnabled = YES;
-    tagView.backgroundColor = [UIColor blackColor];
-    tagView.layer.masksToBounds = YES;
-    tagView.layer.cornerRadius = 5;
-    [self addSubview:tagView];
+    UIButton *tagButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    tagButton.contentEdgeInsets = UIEdgeInsetsMake(1, 5, 1, 5);
+    [tagButton setBackgroundColor:[UIColor blackColor]];
+    tagButton.titleLabel.font = [UIFont systemFontOfSize:12];
+    [tagButton setTitle:text forState:UIControlStateNormal];
+    [tagButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    tagButton.layer.masksToBounds = YES;
+    tagButton.layer.cornerRadius = 5;
+    [self addSubview:tagButton];
     
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(clickTag:)];
-    [tagView addGestureRecognizer:tap];
-    
-    UILabel *textLabel = [[UILabel alloc] init];
-    textLabel.font = [UIFont systemFontOfSize:12];
-    textLabel.textColor = [UIColor whiteColor];
-    textLabel.text = text;
-    [tagView addSubview:textLabel];
-    [textLabel mas_makeConstraints:^(MASConstraintMaker *make)
-    {
-        make.edges.mas_equalTo(UIEdgeInsetsMake(5, 5, 5, 5));
-    }];
-    return tagView;
+    return tagButton;
 }
 -(void)reLayoutTextField
 {
-    UIView *lastTagView = [self getLastTagView];
-    [self.enterTextfield mas_remakeConstraints:^(MASConstraintMaker *make) {
-        if (lastTagView)
+    UIView *lastTagButton = [self getLastTagButton];
+    [self.enterTextfield mas_updateConstraints:^(MASConstraintMaker *make) {
+        if (lastTagButton)
         {
-            make.left.mas_equalTo(lastTagView.mas_right).offset(5);
-            make.top.mas_equalTo(lastTagView.mas_top).offset(0);
-            make.height.equalTo(lastTagView.mas_height);
+            make.left.mas_equalTo(lastTagButton.mas_right).offset(5);
+            make.top.mas_equalTo(lastTagButton.mas_top).offset(0);
+            make.height.equalTo(lastTagButton.mas_height);
         }else
         {
             make.left.equalTo(self.mas_left).offset(0);
             make.top.equalTo(self.mas_top).offset(0);
             make.height.mas_equalTo(@30);
         }
-        make.width.mas_equalTo(self.frame.size.width / 2);
     }];
 }
--(void)clickTag:(UITapGestureRecognizer *)sender
+-(void)clickTag:(UIButton *)sender
 {
-    self.selectedTagView = sender.view;
-    self.selectedTagView.backgroundColor = [UIColor redColor];
+    
 }
 -(void)insterTagWith:(NSString *)tag
 {
-    UIView *lastView = [self getLastTagView];
-    
-    UIView *tagView  = [self tagWithText:tag];
-    [self addSubview:tagView];
-    [tagView mas_makeConstraints:^(MASConstraintMaker *make) {
-        if (lastView) {
-            make.left.equalTo(lastView.mas_right).offset(5);
-            make.centerY.equalTo(lastView.mas_centerY).offset(0);
+    UIButton *lastTagButton = [self getLastTagButton];
+    UIButton *tagButton = [self tagButtonWithText:tag];
+    [self addSubview:tagButton];
+    [tagButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        if (lastTagButton) {
+            make.left.equalTo(lastTagButton.mas_right).offset(5);
+            make.top.equalTo(lastTagButton.mas_top).offset(0);
         }else{
             make.left.equalTo(self.mas_left).offset(0);
             make.top.equalTo(self.mas_top).offset(0);
         }
     }];
     self.enterTextfield.text = @"";
-    [self.tagViews addObject:tagView];
+    [self.tagButtons addObject:tagButton];
     [self reLayoutTextField];
 }
--(UIView *)getLastTagView{
-    return [self.tagViews lastObject];
+-(TLTextField *)enterTextfield{
+    if (!_enterTextfield) {
+        self.enterTextfield = [[TLTextField alloc] init];
+        self.enterTextfield.font = [UIFont systemFontOfSize:12];
+        WS(weakSelf);
+        self.enterTextfield.backward = ^(){
+            if ([weakSelf.enterTextfield.text isEqualToString:@""])
+            {
+                UIButton *lastTagButton = [weakSelf getLastTagButton];
+                if (lastTagButton.selected)
+                {
+                    [[weakSelf getLastTagButton] removeFromSuperview];
+                    [weakSelf.tagButtons removeLastObject];
+                    [weakSelf reLayoutTextField];
+                }else
+                {
+                    [lastTagButton setBackgroundColor:[UIColor redColor]];
+                    lastTagButton.selected = YES;
+                }
+            }
+        };
+        self.enterTextfield.didReturn = ^(NSString *text)
+        {
+            [weakSelf insterTagWith:text];
+        };
+        self.enterTextfield.didEditing = ^()
+        {
+            UIButton *lastTagButton = [weakSelf getLastTagButton];
+            lastTagButton.selected = NO;
+            [lastTagButton setBackgroundColor:[UIColor blackColor]];
+        };
+        return _enterTextfield;
+    }
+    return _enterTextfield;
+}
+-(UIButton *)getLastTagButton{
+    return [self.tagButtons lastObject];
 }
 @end
