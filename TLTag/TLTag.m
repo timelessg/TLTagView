0d8b7e28f6a69f1d181243dcf2981d282e0a404f
Garry <garry@GarrydeiMac.local>
2015-10-29 11:01:55 +0800

    +基本完成 next： 1.增加定制接口 2.点击删除任意Tag 3.加单行/多行模式 4.加注释

--- TLTag/TLTag.m
+++ TLTag/TLTag.m
@@ -52,9 +52,9 @@
     NSUInteger tmpLineCount = 0;
     BOOL isBreak = NO;
     for (int i = 0; i < self.tags.count; i ++) {
-        UIButton *tagView = [self tagButtonWithText:self.tags[i]];
-        [self addSubview:tagView];
-        [tagView mas_makeConstraints:^(MASConstraintMaker *make) {
+        UIButton *tagButton = [self tagButtonWithText:self.tags[i]];
+        [self addSubview:tagButton];
+        [tagButton mas_makeConstraints:^(MASConstraintMaker *make) {
             if (lastTagButton)
             {
                 if (isBreak)
@@ -72,8 +72,8 @@
                 make.top.equalTo(self.mas_top).offset(0);
             }
         }];
-        [tagView layoutIfNeeded];
-        tagsWidth += tagView.bounds.size.width + 5;
+        [tagButton layoutIfNeeded];
+        tagsWidth += tagButton.bounds.size.width + 5;
         if (self.frame.size.width < tagsWidth)
         {
             lineCount = (NSUInteger)tagsWidth / self.frame.size.width;
@@ -86,8 +86,8 @@
                 isBreak = NO;
             }
         }
-        lastTagButton = tagView;
-        [self.tagButtons addObject:tagView];
+        lastTagButton = tagButton;
+        [self.tagButtons addObject:tagButton];
     }
     if (self.mode == TLTagModeEdit)
     {
@@ -119,14 +119,12 @@
     [tagButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
     tagButton.layer.masksToBounds = YES;
     tagButton.layer.cornerRadius = 5;
-    [self addSubview:tagButton];
-    
     return tagButton;
 }
 -(void)reLayoutTextField
 {
-    UIView *lastTagButton = [self getLastTagButton];
-    [self.enterTextfield mas_updateConstraints:^(MASConstraintMaker *make) {
+    UIButton *lastTagButton = [self getLastTagButton];
+    [self.enterTextfield mas_remakeConstraints:^(MASConstraintMaker *make) {
         if (lastTagButton)
         {
             make.left.mas_equalTo(lastTagButton.mas_right).offset(5);
@@ -138,6 +136,7 @@
             make.top.equalTo(self.mas_top).offset(0);
             make.height.mas_equalTo(@30);
         }
+        make.width.mas_equalTo(self.frame.size.width / 2);
     }];
 }
 -(void)clickTag:(UIButton *)sender
@@ -147,15 +146,24 @@
 -(void)insterTagWith:(NSString *)tag
 {
     UIButton *lastTagButton = [self getLastTagButton];
+    [lastTagButton layoutIfNeeded];
+    CGFloat tagButtonWidth = [tag sizeWithAttributes:@{NSFontAttributeName:[UIFont systemFontOfSize:12]}].width + 10;
+    CGFloat lastTagOffsetX = lastTagButton.bounds.size.width + lastTagButton.frame.origin.x + 5;
+    
     UIButton *tagButton = [self tagButtonWithText:tag];
     [self addSubview:tagButton];
     [tagButton mas_makeConstraints:^(MASConstraintMaker *make) {
-        if (lastTagButton) {
-            make.left.equalTo(lastTagButton.mas_right).offset(5);
-            make.top.equalTo(lastTagButton.mas_top).offset(0);
-        }else{
+        if (lastTagOffsetX + tagButtonWidth > self.frame.size.width) {
             make.left.equalTo(self.mas_left).offset(0);
-            make.top.equalTo(self.mas_top).offset(0);
+            make.top.equalTo(lastTagButton.mas_bottom).offset(5);
+        }else{
+            if (lastTagButton) {
+                make.left.equalTo(lastTagButton.mas_right).offset(5);
+                make.top.equalTo(lastTagButton.mas_top).offset(0);
+            }else{
+                make.left.equalTo(self.mas_left).offset(0);
+                make.top.equalTo(self.mas_top).offset(0);
+            }
         }
     }];
     self.enterTextfield.text = @"";
@@ -163,11 +171,13 @@
     [self reLayoutTextField];
 }
 -(TLTextField *)enterTextfield{
-    if (!_enterTextfield) {
+    if (!_enterTextfield)
+    {
         self.enterTextfield = [[TLTextField alloc] init];
         self.enterTextfield.font = [UIFont systemFontOfSize:12];
         WS(weakSelf);
-        self.enterTextfield.backward = ^(){
+        self.enterTextfield.backward = ^()
+        {
             if ([weakSelf.enterTextfield.text isEqualToString:@""])
             {
                 UIButton *lastTagButton = [weakSelf getLastTagButton];
